b2bdb8a85d721763ad54bc7d943feae04bd76e09
Nitish <nitishnbt0452@gmail.com>
2025-05-13 01:02:30 +0530

    To Do list Made added Delete Button

---
+++ script.js
@@ -0,0 +1,26 @@
+let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
+btn.addEventListener("click", () => {
+  {
+
+    let ch = document.createElement("li");
+    ch.innerText = inp.value;
+    let butn = document.createElement("button");
+    inp.value = " ";
+    butn.innerText = "Delete";
+    ul.appendChild(ch);
+    ul.appendChild(butn);
+    
+
+    butn.addEventListener("click", () => {
+      ch.remove();
+      butn.remove();
+
+    });
+  }
+});
+
+inp.addEventListener("click", ()=>{
+   inp.placeholder = " ";
+})
