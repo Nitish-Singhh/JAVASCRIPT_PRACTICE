2d0c84264e18e5fd3db9cae27eebff60d79115aa
Nitish <nitishnbt0452@gmail.com>
2025-05-13 23:03:31 +0530

    modefied Delete Button in it

--- script.js
+++ script.js
@@ -2,23 +2,27 @@ let btn = document.querySelector("button");
 let ul = document.querySelector("ul");
 let inp = document.querySelector("input");
 btn.addEventListener("click", () => {
-  {
+  
 
     let ch = document.createElement("li");
     ch.innerText = inp.value;
+
     let butn = document.createElement("button");
-    inp.value = "";
+    // inp.value = "";
     butn.innerText = "Delete";
-    ul.appendChild(ch);
-    ul.appendChild(butn);
+    
     
 
     butn.addEventListener("click", () => {
       ch.remove();
-      butn.remove();
+      // butn.remove();
 
     });
-  }
+
+    ch.appendChild(butn);
+    ul.appendChild(ch);
+    inp.value = "";
+  
 });
 
 inp.addEventListener("click", ()=>{
